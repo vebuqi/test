cb928a4010bd4a6bd7fc903a53e13b764e34fce7
vebuqi <vebuqi@163.com>
2015-01-31 00:34:37 +0800

    add new methods
    add some new methods including getRelatedTarget(),getButton() and
    getWheelDelta().These three methods are all about mouse-events.

--- event.js
+++ event.js
@@ -29,5 +29,42 @@ var EventUtil = {
 		} else {
 			event.cancelBubble = true;
 		}
+	},
+
+	getRelatedTarget: function(event) {
+		if (event.relatedTarget) {
+			return event.relatedTarget;
+		} else if (event.fromrElement) {
+			return event.fromElement;
+		} else if (event.toElement) {
+			return event.toElement;
+		}
+	},
+
+	getButton: function(event) {
+		if (document.implementation.hasFeature("MouseEvents", 2.0)) {
+			return event.button;
+		} else {
+			switch(event.button) {
+				case: 0
+				case: 1
+				case: 3
+				case: 5
+				case: 7 return 0;
+
+				case: 2
+				case: 6 return 1;
+
+				case: 4 return 2;
+			}
+		}
+	},
+
+	getWheelDelta: function(event) {
+		if (event.wheelDelta) {
+			return event.wheelDelta;
+		} else {
+			return -event.detail * 40;
+		}
 	}
 };
